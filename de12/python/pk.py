511787c7391d17e6436e7370a3073843f7e6403d
LeeSuzu <r202302265bp@jindai.jp>
2023-10-30 14:36:39 +0900

    20231030

--- de12/python/pk.py
+++ de12/python/pk.py
@@ -3,7 +3,9 @@ name=input("プレイヤー名を入力してください。")
 print(name,"さんは、キッカーです。キーパーを出し抜いてシュートを決めよう！")
 point=0
 
-for i in range(1,5):
+# 5回の試合を実行
+for i in range(1,6):
+     # プレイヤーがPKを蹴る
  print(i,"戦目")
  print("１．左上　２．左下　３．真ん中　４．右上　５．右下")
  serect=int(input("蹴りたい場所の数字を入力してね"))
@@ -17,7 +19,10 @@ for i in range(1,5):
    print("ナイスシュートだー‼︎")
    point=point+1
 
-   if point>=3:
-    print(name,"さんの結果は...",point,"点獲得で勝利！")
-   elif point<=2:
-    print(name,"さんの結果は...",point,"点獲得で敗退...")
+    # ゴール数を確認して勝敗を表示
+if point >= 3:
+        print(f"ゴール数: {point} - 点獲得で勝利！！")
+else:
+        print(f"ゴール数: {point} - 残念ながら敗退...。")
+
+    
